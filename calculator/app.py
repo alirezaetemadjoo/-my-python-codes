462bb9beb7f22aa057ad1b5220c85bb1dfb3ea48
Alireza Etemadjoo <alirezaetemadjoo23@gmail.com>
2026-06-10 11:09:22 +0330

    Hi guys
    new updates for call app

---
+++ calculator/app.py
@@ -0,0 +1,15 @@
+#--------------------stater---------------------
+
+from tkinter import*
+import tkinter.messagebox
+
+#--------------------setting--------------------
+
+call = Tk()
+call.title("Calculator")
+call.geometry("350x450")
+call.resizable(False,False)
+color = "black"
+call.configure(bg = color)
+PhotoImage(file = "")
+
